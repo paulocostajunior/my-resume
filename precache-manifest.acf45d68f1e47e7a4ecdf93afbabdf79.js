96203a517d3a917feaf627bfd6e9ef5d04a2f051
Paulo Costa Jr <costapaulojr@gmail.com>
2019-07-11 23:17:46 -0300

    Updates

---
+++ precache-manifest.acf45d68f1e47e7a4ecdf93afbabdf79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6101c5cf5215ae6f4fcc20b8c0897fc",
+    "url": "/my-resume/index.html"
+  },
+  {
+    "revision": "8f8914647433623b6b94",
+    "url": "/my-resume/static/css/main.4d1eea28.chunk.css"
+  },
+  {
+    "revision": "88ecef5e8cb724f0473f",
+    "url": "/my-resume/static/js/2.6459c444.chunk.js"
+  },
+  {
+    "revision": "8f8914647433623b6b94",
+    "url": "/my-resume/static/js/main.01341968.chunk.js"
+  },
+  {
+    "revision": "2f7b259ab1933d650523",
+    "url": "/my-resume/static/js/runtime~main.1423441b.js"
+  },
+  {
+    "revision": "3b6aad93e4970d2ff5474fd36ed84aed",
+    "url": "/my-resume/static/media/my-photo.3b6aad93.png"
+  }
+]);
